086e10c6ac63a720ce2118820b5bbbac03cd5ec6
Joahan Basurto <joahan@ad.unc.edu>
2021-02-13 21:38:12 -0500

    Exercise 2 Pt.2

--- exercises/ex02_p2/vaccine_calc.py
+++ exercises/ex02_p2/vaccine_calc.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime, timedelta
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "930410205"
 
 
 def main() -> None:
@@ -11,16 +11,24 @@ def main() -> None:
     doses: int = int(input("Doses administered: "))
     doses_per_day: int = int(input("Doses per day: "))
     target: int = int(input("Target percent vaccinated: "))
-    # TODO 2: Call days_to_target and store result in a variable.
-    # TODO 4: Call future_date and store result in a variable.
-    # TODO 5: Print the expected output using the variables above.
+    days_until: int = days_to_target(population, doses, doses_per_day, target)
+    days_left: str = future_date(days_until)
+    print("We will reach " + str(target) + "% vaccination in " + str(days_until) + " days, which falls on " + days_left)
 
 
-# TODO 1: Define days_to_target function
+def days_to_target(population: int, doses: int, doses_per_day: int, target: int) -> int:
+    days_needed = ((((2 * (target / 100) * (population)) - doses)) / (doses_per_day))
+    days_needed = round(days_needed)
+    return int(days_needed)
 
 
-# TODO 3: Define future_date function
+def future_date(days: int) -> str:
+    today: datetime = datetime.today()
+    days_until_vaccination: timedelta = timedelta(days)
+    vaccination_date: datetime = today + days_until_vaccination
+    dia_de_vacuna: str = str(vaccination_date.strftime("%B %d, %Y"))
+    return str(dia_de_vacuna)
 
-
+    
 if __name__ == "__main__":
     main()
